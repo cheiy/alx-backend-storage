bc020973b24f4d11feb4a1abee91652b952ab802
cheiy <gschege@gmail.com>
2023-08-17 02:54:24 +0000

    Key

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,12 +15,10 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    @call_history
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores a value in Redis and returns a unique key
         """
         key = str(uuid.uuid4())
-        self._redis.set(data_key, data)
+        self._redis.set(key, data)
         return key
