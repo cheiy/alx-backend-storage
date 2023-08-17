949158f62aedaa93f6441ebbad40e68c12ab5dd9
cheiy <gschege@gmail.com>
2023-08-17 13:48:11 +0000

    Reading from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,3 +22,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) ->\
+            Union[str, bytes, int, float]:
+        """
+        Method takes a key string argument and an optional Callable argument
+        """
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """
+        Method automatically parameterizes Cache.get with the correct
+        conversion function for strings - in this case decode
+        """
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """
+        Method automatically parameterizes Cache.get with the correct
+        conversion function for integers
+        """
+        return self.get(key, lambda x: int(x))
