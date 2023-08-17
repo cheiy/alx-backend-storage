12fbeb8a856107b228e750791099157fb8f68ecd
cheiy <gschege@gmail.com>
2023-08-17 02:51:50 +0000

    Exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,8 @@
 Module contains the Cache class
 """
 import redis
+import uuid
+from typing import Union
 
 
 class Cache:
@@ -12,3 +14,13 @@ class Cache:
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Stores a value in Redis and returns a unique key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return key
