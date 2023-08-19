25447efd77b2be28281239946996f571b523810f
cheiy <gschege@gmail.com>
2023-08-19 02:23:14 +0000

    Update topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,5 +9,4 @@ def update_topics(mongo_collection, name, topics):
     Function changes all topics of a school document based on the name
     """
     mongo_collection.update_many({"name": name},
-                                {"$push": {"topics":
-                                           {"$each": topics}}})
+                                 {"$set": {"topics": topics}})
